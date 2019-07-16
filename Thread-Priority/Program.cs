ac92f2def8f79542141fb22710ce399631adc206
amir <amir.karshenas@gmail.com>
2019-07-15 22:42:04 -0400

    Thread-Priority

---
+++ Thread-Priority/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Priority {
+
+    class Program {
+
+        public static void ThreadMethod () {
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine (" ThreadMethod: {0}", i);
+                Thread.Sleep (0);
+            }
+        }
+        static void Main (string[] args) {
+            Thread t = new Thread (new ThreadStart (ThreadMethod));
+            t.Start ();
+
+            for (int i = 0; i < 4; i++) {
+                Console.WriteLine (" MainThread  ");
+            }
+
+            t.Join();
+            
+        }
+    }
+}
